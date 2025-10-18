5a8a8fe88bc87eb62dc16cf37110949eb8ad5398
Cleiidson <cleidson.ls12@gmail.com>
2025-10-18 16:15:03 -0300

    implementando a questão do tempo

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // 1. Definição de Constantes e Elementos DOM
     const REGISTRO_FORM_ID = 'registroForm';
     // Mantenha sua URL REAL aqui
-    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxlUJdVjMmNIkavN1UiHCrvvRpuUXrYUu63yP9QztZ-AMrdmQO1yPfQ-REwVN-y8sOJog/exec'; 
+    const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzd5POz2oY5CEnSswSqSgV8_y_ZOkTrzP8G45jYKHU9Sc3_-tMRKFP9Z9IHqaTPOAumDg/exec'; 
     const LEITO_EM_ANDAMENTO_KEY = 'leitosEmAndamento'; // CHAVE PARA O ARRAY DE LEITOS ATIVOS
     const MODO_ATUAL_KEY = 'modoRegistro'; 
 
